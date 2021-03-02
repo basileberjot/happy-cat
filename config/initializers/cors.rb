af0ee8c5ef12d5154308e423b929e3cc9acc7285
Basile Berjot <basileberjot@gmail.com>
2021-03-02 19:00:13 +0900

    cors allow all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,16 +9,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
-
-  allow do
-    origins "https://happy-cat.heroku.com"
+    origins "*"
 
     resource '*',
       headers: :any,
